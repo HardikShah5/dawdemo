885a93dc5b31052bddbe1e70f6afb49815bbf9b9
hardik <Alept@Hardiks-MacBook-Pro.local>
2016-11-29 09:36:56 +0530

    commit for gallery view

---
+++ Dawriyah/GalleryView.swift
@@ -0,0 +1,55 @@
+//
+//  GalleryView.swift
+//  Dawriyah
+//
+//  Created by Hardik Shah on 29/11/16.
+//  Copyright © 2016 MehulS. All rights reserved.
+//
+
+import UIKit
+
+class GalleryView: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellIdentifier = "GalleryViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        
+        
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+    
+}
---
+++ Dawriyah/GalleryViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryViewCell.swift
+//  Dawriyah
+//
+//  Created by Hardik Shah on 29/11/16.
+//  Copyright © 2016 MehulS. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewCell: UICollectionViewCell {
+    
+}

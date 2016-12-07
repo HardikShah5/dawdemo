4f939884c9c4daa20a667c83176ee6b1ff322297
hardik <Alept@Hardiks-MacBook-Pro.local>
2016-12-08 00:31:30 +0530

    commit for missing files

---
+++ Dawriyah/Constants/AppUtils.swift
@@ -0,0 +1,55 @@
+//
+//  AppUtils.swift
+//  WhiteLeafBooks
+//
+//  Created by MehulS on 10/07/16.
+//  Copyright © 2016 MehulS. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AppUtils: NSObject {
+    
+    
+    //MARK: App Delegate Object
+    static func APPDELEGATE() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    
+    //MARK: Validations For Email
+    static func validateEmail(_ strEmail : String)-> Bool {
+        let emailRegex : String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        
+        return emailTest.evaluate(with: strEmail)
+    }
+    
+        
+    //For language text
+    static func localized(_ key: String, value: String) -> String {
+        var lang: String! = "en"
+        
+        let arrLang: NSArray! = UserDefaults.standard.value(forKey: "AppleLanguages") as! NSArray
+        if arrLang.count > 0 {
+            lang = arrLang.firstObject as! String
+            
+            if lang.contains("-") {
+                lang = lang.components(separatedBy: "-").first
+            }
+        }
+        
+        let path = Bundle.main.path(forResource: lang, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        
+        return NSLocalizedString(key, tableName: nil, bundle: bundle!, value: value, comment: "")
+    }
+    
+    
+    //Current Language
+    static func currentLanguage() -> String {
+        let langStr = Locale.current.languageCode
+        return langStr!
+    }
+}
---
+++ Dawriyah/Constants/Constants.swift
@@ -0,0 +1,54 @@
+//
+//  Constants.swift
+//  Dawriyah
+//
+//  Created by MehulS on 03/12/16.
+//  Copyright © 2016 MehulS. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+    
+    //Colors
+    struct Color {
+        static let BLUE_UNDERLINE   =   UIColor(colorLiteralRed: 30/255.0, green: 126/255.0, blue: 233/255.0, alpha: 1.0)
+    }
+
+    //Storyboard Identifier
+    struct StoryboardIdentifier {
+        static let HOME                     = "Home"
+        static let FACEBOOK                 = "FBTwitterPost"
+        static let TWITTER                  = "FBTwitterPost"
+        static let FACEBOOK_DETAILS         = "FBTWProfile"
+        static let TWITTER_DETAILS          = "FBTWProfile"
+        static let NEWS_LISTING             = "NewsListing"
+        static let NEWS_DETAILS             = "NewsDetail"
+    }
+}
+
+//Slider Menu Options
+struct SliderMenuOption {
+    static let PROFILE              = 0
+    static let ADVANCED_SEARCH      = 1
+    static let NEWS                 = 2
+    static let PRESS_KIT            = 3
+    static let TWITTER              = 4
+    static let FACEBOOK             = 5
+    static let ANNUAL_REPORT        = 6
+    static let NEWS_PAPER           = 7
+    static let OFFICIAL_MAGAZINE    = 8
+    static let THE_BOOK             = 9
+    static let STATISTICS           = 10
+    static let FAVORITE             = 11
+    static let ABOUT_US             = 12
+    static let CONTACT_US           = 13
+    static let HELP                 = 14
+    static let LOGOUT               = 15
+}
+
+//Current Language
+struct CurrentLanguage {
+    static let ENGLISH  =   "en"
+    static let ARABIC   =   "ar"
+}

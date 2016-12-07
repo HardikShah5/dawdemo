691efd8600f248e9a3c54074f67c8b9955926457
hardik <Alept@Hardiks-MacBook-Pro.local>
2016-12-07 23:43:16 +0530

    Commit for OfficialMagazieListing view

---
+++ Dawriyah/OfficialMagazineCell.swift
@@ -0,0 +1,24 @@
+//
+//  OfficialMagazineCell.swift
+//  Dawriyah
+//
+//  Created by Hardik Shah on 07/12/16.
+//  Copyright © 2016 MehulS. All rights reserved.
+//
+
+import UIKit
+
+class OfficialMagazineCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
